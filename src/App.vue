cb952fd8c4dde6619906da0b18d99b185199b294
hollisyang <hollis2002@qq.com>
2024-04-25 22:56:35 +0800

    init vue-cesium

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import * as Cesium from 'cesium'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0MGMyMWM0YS1lNDEyLTRjNWYtYTY3OS1jOTZkOWM1OThjYTEiLCJpZCI6MTkxMTMxLCJpYXQiOjE3MDU5MDY1ODN9.Tq3eObtuZJiqt4rDl-srQkMfz-WP9_EvKEPa_UvOI5s'
+
+  const viewer = new Cesium.Viewer('cesiumContainer', {
+    animation: false,
+    baseLayerPicker: false,
+    fullscreenButton: false,
+    vrButton: false,
+    geocoder: false,
+    homeButton: false,
+    infoBox: false,
+    sceneModePicker: false,
+    selectionIndicator: false,
+    timeline: false,
+    navigationHelpButton: false,
+    navigationInstructionsInitiallyVisible: false,
+  })
+
+  viewer._cesiumWidget._creditContainer.style.display = 'none'
+})
+</script>
+
+<template>
+  <div id="cesiumContainer">
+  </div>
+</template>
+
+<style scoped>
+#cesiumContainer {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+}
+</style>
